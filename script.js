2cd702ec677be24ca11a49e959774f81bc342f15
AntonSmid <anton.smid@triera.net>
2019-01-19 21:38:12 +0100

    Owl Carousel added

--- script.js
+++ script.js
@@ -8,13 +8,34 @@ $(document).ready(function() {
   
   // nav btn
   $(".nav-btn").click(function() {
+    $("main").toggleClass("max-width");
+    
     $(".side-nav").toggleClass("margin");
     $(".nav-btn").toggleClass("change");
-    $("main").toggleClass("max-width");
+    
   })
   
   
-  
+  $('.songs-center').owlCarousel({
+    loop:true,
+    margin:10,
+    smartSpeed: 2000,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        },
+        1400:{
+            items:7
+        }
+    }
+})
   
   
   
