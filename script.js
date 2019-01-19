273bbb23be289987cf0bf41923a275eff2533884
AntonSmid <anton.smid@triera.net>
2019-01-19 16:49:22 +0100

    main nav - added JS and CSS

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
  
+  // side btn
  $(".side-btn").click(function(){
-   $(".side-nav-links").slideToggle(3000);
+   $(".side-nav-links").slideToggle(1000);
    $(".side-btn").toggleClass("change");
  }) 
+  
+  // nav btn
+  $(".nav-btn").click(function() {
+    $(".side-nav").toggleClass("margin");
+    $(".nav-btn").toggleClass("change");
+    $("main").toggleClass("max-width");
+  })
   
   
   
